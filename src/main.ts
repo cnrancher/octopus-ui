c0882a1fdf0a8a8d6b6762c80a95168e681f09ac
loganhz <logan@rancher.com>
2020-02-18 22:01:24 +0800

    Add axios

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './plugins/element';
+import './plugins/axios';
 import i18n from './plugins/i18n';
 
 Vue.config.productionTip = false;

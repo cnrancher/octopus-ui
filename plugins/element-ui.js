5cfec9b9105c0231108b3bf9ab2c2043ce3b9cb2
wj <897415845@qq.com>
2020-04-04 18:01:42 +0800

    fix https error

--- plugins/element-ui.js
+++ plugins/element-ui.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-// import '@/assets/styles/base/element-variables.scss'
 
-Vue.use(ElementUI);
+// @TODO: custom theme
+// import '@/assets/styles/base/element-variables.scss'
+Vue.use(ElementUI, { size: 'small', zIndex: 1000 });
